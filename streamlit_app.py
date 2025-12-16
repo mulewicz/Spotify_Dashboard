e186d4c7dad991c15f6d31dd572ecc5dcb6e8a50
mulewicz <aleksandramulewicz@gmail.com>
2025-12-16 12:06:02 +0100

    push

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import plotly.express as px
 from streamlit_option_menu import option_menu
 import sys
+import lyricsgenius
+from wordcloud import WordCloud, STOPWORDS
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from streamlit_lottie import st_lottie
@@ -13,10 +15,12 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from utils import *
+import spotipy
 import pickle
 colors_maciek = ['#1d2026', '#172554', '#1e40af', '#3b82f6', '#60a5fa', '#93c5fd']
 colors_ola = ['#1d2026', '#2e1065', '#7c3aed','#c084fc','#e9d5ff','#dcd0ff']
-
+colors_maciek_light = ['#60a5fa', '#93c5fd', '#bfdbfe', '#dbeafe', "#008080", "#b2d8d8", "#005b96", "#92d2f9", "#344771"]*20
+colors_ola_light = ['#a78bfa', '#c084fc', '#e9d5ff','#f3e8ff', "#9F2B68", "#D8BFD8", "#660066", "#800080", "#9f72ca"]*20
 with open('data/dane_ola.pkl', 'rb') as file:
     data_ola = pickle.load(file)
 
@@ -25,7 +29,6 @@ with open('data/dane_maciek.pkl', 'rb') as file:
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STYLE_PATH = os.path.join(BASE_DIR, "style.css")
-
 
 st.set_page_config(layout="wide")
 st.write("<style>" + open(STYLE_PATH).read() + "</style>", unsafe_allow_html=True)
@@ -42,35 +45,23 @@ st.markdown('<div class="big-title">Spotify Dashboard Overview</div>', unsafe_al
 st.caption("This app was created to analyse trends present in our music taste")
 
 if selected == "Home":
-  col1, col2= st.columns((2,3))
-
-  with col1:
-    st.markdown('<div class="card">', unsafe_allow_html=True)
-    st.markdown('<div class="metric-label">Total time listened</div>', unsafe_allow_html=True)
-    st.markdown('<div class="metric-value">3,049K minutes</div>', unsafe_allow_html=True)
-    st.markdown('<div class="chip"><div class="chip-dot"></div>+100 min compared to 2023</div>', unsafe_allow_html=True)
-    st.caption("this adds upp to 120 days")
-    st.markdown('</div>', unsafe_allow_html=True)
-
-  with col2:
-    st.markdown('<div class="card">', unsafe_allow_html=True)
-    st.markdown("**Our Favourite albums**")
-
-    st.markdown('</div>', unsafe_allow_html=True)
-
-  bottom_left, bottom_right = st.columns((3,2))
-
-  st.title("Music Activity Calendar")
+  col_1, col_2= st.columns((2,3))
+  art1, art2 = st.columns([1, 1.5], gap="large")
+  st.markdown('<div class="card">', unsafe_allow_html=True)
+  st.markdown('<div class="big-title">Music Activity calendar</div>', unsafe_allow_html=True)
   col1, col2 = st.columns([3, 1])
   with col2:
       sel_person = st.selectbox("Choose user",[ "Maciek", "Ola"])
       if sel_person == "Ola":
-          data = data_ola
+          df = data_ola
           colors = colors_ola
+          colors_light = colors_ola_light
       else:
-          data = data_maciek
+          df = data_maciek
           colors = colors_maciek
-
+          colors_light = colors_maciek_light
+      df['ts_date'] = pd.to_datetime(df['ts_date'])
+      data = df.copy()
       activity = data.groupby("ts_date")['ms_played'].sum()
       data = activity.reset_index() if isinstance(activity, pd.Series) else activity.copy()
       data['ts_date'] = pd.to_datetime(data['ts_date'])
@@ -83,28 +74,146 @@ if selected == "Home":
   with col1:
       st.plotly_chart(draw_chart(data, colors, sel_year, view_mode), use_container_width=True)
 
+  with col_1:
+      st.markdown('<div class="card">', unsafe_allow_html=True)
+      top_artists_df, title = get_top_5_artists(df, sel_year, view_mode)
+      st.caption(title)
+      rank_color = colors[3]
+      for idx, row in top_artists_df.reset_index(drop=True).iterrows():
+          rank = idx + 1
+          artist_name = row['master_metadata_album_artist_name']
+          count = row['counts']
+          img_url, spotify_link = get_artist_image_url(artist_name)
+          if not img_url: img_url = "https://via.placeholder.com/50"
+          html_code = f"""
+                <a href="{spotify_link}" target="_blank" style="text-decoration: none;">
+                    <div class="artist-row">
+                        <span style="font-size: 1.2rem; font-weight: bold; color: {rank_color}; width: 30px;">#{rank}</span>
+                        <img src="{img_url}" class="artist-img">
+                        <div class="artist-info">
+                            <p class="artist-name">{artist_name}</p>
+                            <p class="artist-stats">{count} odtworzeń</p>
+                        </div>
+                        <div class="spotify-icon">➤</div>
+                    </div>
+                </a>
+                """
+          st.markdown(html_code, unsafe_allow_html=True)
+
+
+
+  with col_2:
+      st.markdown('<div class="card">', unsafe_allow_html=True)
+
+      top_albums_df, title = get_top_8_albums(df, sel_year, view_mode)
+      st.caption(title)
+      cols = st.columns(4)
+
+      for i, (idx, row) in enumerate(top_albums_df.iterrows()):
+          album_name = row['master_metadata_album_album_name']
+          artist_name = row['master_metadata_album_artist_name']
+          cover_url, _ = get_artist_image_url(artist_name)
+          if not cover_url: cover_url = "https://via.placeholder.com/150"
+
+          with cols[i % 4]:
+              st.markdown(f"""
+                    <div class="album-grid-item">
+                        <img src="{cover_url}" class="album-cover">
+                        <div class="album-title-text" title="{album_name}">{album_name}</div>
+                        <div class="album-artist-text">{artist_name}</div>
+                    </div>
+                    """, unsafe_allow_html=True)
+
+  bottom_left, bottom_right = st.columns((2,3))
   with bottom_left:
     st.markdown('<div class="card">', unsafe_allow_html=True)
-    st.write("**minutes listened per month (maciek fiolet, ola rozowy)*")
-    df = pd.DataFrame({
-        "month": ["Jan","Feb","Mar","Apr","May","Jun"],
-        "minutes listened": [2200, 2450, 3049, 2800, 2900, 3100]
-    })
-    fig = px.bar(df, x="month", y="minutes listened")
-    fig.update_layout(template="plotly_dark", height=260, margin=dict(l=0,r=0,t=20,b=0))
+    fig = chart_sum(data, sel_year, colors[3])
     st.plotly_chart(fig, use_container_width=True)
     st.markdown('</div>', unsafe_allow_html=True)
 
   with bottom_right:
     st.markdown('<div class="card">', unsafe_allow_html=True)
-    st.write("**Top genre per season**")
-    table = pd.DataFrame({
-        "Season": ["Spring","Summer","Autumn","Winter"],
-        "Ola": ["musicals","rock","indie","alternative"],
-        "Maciek": ["rap","alternative","pop","rap"]
-    })
-    st.dataframe(table, use_container_width=True, hide_index=True)
-    st.markdown('</div>', unsafe_allow_html=True)
+
+    top_songs, title = get_top_5_songs(df, sel_year, view_mode)
+    st.caption(title)
+    for index, row in top_songs.reset_index(drop=True).iterrows():
+        rank = index + 1
+        title = row['master_metadata_track_name']
+        artist = row['master_metadata_album_artist_name']
+        plays = row['counts']
+
+        rank_color = colors[3]
+
+        st.markdown(f"""
+        <div style="
+            background-color: #11141d;
+            border-radius: 10px;
+            padding: 10px 15px;
+            margin-bottom: 8px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        ">
+            <div style="display: flex; align-items: center; gap: 15px;">
+                <span style="font-size: 1.2rem; font-weight: bold; color: {rank_color}; width: 30px;">#{rank}</span>
+                <div>
+                    <div style="color: white; font-weight: 600; font-size: 0.95rem;">{title}</div>
+                    <div style="color: #8f9bb3; font-size: 0.8rem;">{artist}</div>
+                </div>
+            </div>
+            <div style="font-family: monospace; color: #ccc; font-size: 0.9rem; background: #262730; padding: 2px 8px; border-radius: 6px;">
+                {plays}
+            </div>
+        </div>
+        """, unsafe_allow_html=True)
+
+  met1, met2, met3, met4 = st.columns(4)
+
+  with met1:
+      diff = get_max_in_the_day(df, sel_year) - get_max_in_the_day(df, sel_year-1)
+      st.metric(
+          label=f"Max hours in a day ({sel_year})",
+          value=f"{get_max_in_the_day(df, sel_year):.1f}",
+          delta=f"{diff:+,.1f} h ({sel_year-1})"
+      )
+
+  with met2:
+      diff = num_of_artists_listened(df, sel_year) - num_of_artists_listened(df, sel_year-1)
+      st.metric(
+          label=f"Number of artists listened ({sel_year})",
+          value=f"{num_of_artists_listened(df, sel_year):.0f}",
+          delta=f"{diff:+,.1f} ({sel_year-1})"
+      )
+
+  with met3:
+      diff = get_total_days(df, sel_year) - get_total_days(df, sel_year-1)
+      st.metric(
+          label="Total number of days played",
+          value=f"{get_total_days(df, sel_year):.1f}",
+          delta=f"{diff:+,.1f} days ({sel_year-1})"
+      )
+
+  with met4:
+      st.metric(
+          label="tba",
+          value="76,314",
+          delta="-18.4% from previous week"
+      )
+
+      df_top_data, _ = get_top_5_songs(df, sel_year, view_mode, num=10)
+      top_songs = list(
+          zip(df_top_data['master_metadata_album_artist_name'], df_top_data['master_metadata_track_name']))
+  st.markdown('<div class="card">', unsafe_allow_html=True)
+  if view_mode == "Full Year":
+      title = f"{sel_year}"
+  else:
+      title = f"{sel_year} ({view_mode})"
+  fig = get_lyrics_cloud_plotly(top_songs, colors_light, title)
+
+  if fig:
+          st.plotly_chart(fig, use_container_width=True)
+  else:
+          st.warning("Nie udało się pobrać wystarczającej ilości tekstu.")
 
 if selected == "Stats":
   col1, col2, col3, col4 = st.columns(4)
--- utils.py
+++ utils.py
@@ -1,21 +1,286 @@
 # the code for all plotly figures can be found here
+from typing import Any
+
 import numpy as np
 import plotly.graph_objects as go
 import plotly
 from plotly.subplots import make_subplots
 import pandas as pd
 import streamlit as st
+import lyricsgenius
+from wordcloud import WordCloud, STOPWORDS
+from langdetect import detect, LangDetectException
+from collections import Counter
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import random
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+GENIUS_ACCESS_TOKEN = "3O6Gergq8PJj06gtdB2sfrFwPdEMCgZzaN1rdjumu8Bwu8GVHZsYYgMzBsxUAxeF"
 
-def draw_chart(data, colors, sel_year, view_mode):
-    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+def get_lang(lyrics_list):
+    detected_langs = []
+    for text in lyrics_list:
+            lang_code = detect(text)
+            detected_langs.append(lang_code)
+    stats = Counter(detected_langs)
+    data = []
+    for code, count in stats.items():
+        data.append({'code': code, 'count': count})
+
+    df_stats = pd.DataFrame(data).sort_values(by='count', ascending=False)
+    return df_stats
+
+def get_lyrics_cloud_plotly(artist_song_pairs, colors, title):
+        genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN)
+        genius.verbose = False
+        genius.remove_section_headers = True
+
+        my_stopwords = set(STOPWORDS)
+        my_stopwords.update(['feat', 'ft', 'verse', 'chorus', 'intro', 'outro', 'yeah', 'oh', 'la', 'na', 'ooh'])
+        lyrics_data = []
+        all_lyrics = ""
+
+        progress_bar = st.progress(0)
+        status_text = st.empty()
+
+        for i, (artist, song) in enumerate(artist_song_pairs):
+            status_text.text(f"Downloading {artist} - {song}...")
+            try:
+                song_obj = genius.search_song(song, artist)
+                if song_obj and song_obj.lyrics:
+                    try:
+                        lang_detected = detect(song_obj.lyrics)
+                    except:
+                        lang_detected = "unknown"
+
+                    lyrics_data.append({
+                        "text": song_obj.lyrics,
+                        "lang": lang_detected
+                    })
+            except Exception:
+                pass
+
+            progress_bar.progress((i + 1) / len(artist_song_pairs))
+
+        progress_bar.empty()
+        status_text.empty()
+        available_langs = sorted(list(set([item['lang'] for item in lyrics_data])))
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.markdown('<div class="big-title">Word Cloud</div>', unsafe_allow_html=True)
+            st.caption(title)
+
+        with col4:
+            selected_lang = st.selectbox("Choose language:", available_langs)
+
+        filtered_lyrics = [item['text'] for item in lyrics_data if item['lang'] == selected_lang]
+        all_lyrics = " ".join(filtered_lyrics)
+        if not all_lyrics:
+            return None
+
+        wc = WordCloud(
+            width=1000,
+            height=800,
+            max_words=100,
+            stopwords=my_stopwords,
+            background_color='black'
+        ).generate(all_lyrics)
+
+        words = []
+        #colors = []
+        x_pos = []
+        y_pos = []
+        sizes = []
+        for item in wc.layout_:
+            words.append(item[0][0])
+            sizes.append(item[1])
+            x_pos.append(item[2][1])
+            y_pos.append(item[2][0] * -1)
+
+        word_colors = [random.choice(colors) for _ in words]
+        z_pos = [random.randint(-400, 400) for _ in words]
+
+        fig = go.Figure(go.Scatter3d(
+            x=x_pos,
+            y=y_pos,
+            z=z_pos,
+            mode='text',
+            text=words,
+            hoverinfo='text',
+            hovertext=[f"Słowo: {w}" for w in words],
+            textfont=dict(
+                size=sizes,
+                color=colors,
+                family="Arial"
+            )
+        ))
+
+        axis_layout = dict(
+            showgrid=False,
+            showticklabels=False,
+            zeroline=False,
+            title='',
+            showbackground=False,
+            visible=False
+        )
+
+        fig.update_layout(
+            scene=dict(
+                xaxis=axis_layout,
+                yaxis=axis_layout,
+                zaxis=axis_layout,
+                bgcolor='rgba(0,0,0,0)'
+            ),
+            paper_bgcolor='rgba(0,0,0,0)',
+            margin=dict(l=0, r=0, t=0, b=0),
+            hovermode='closest'
+        )
+
+        return fig
+
+def get_artist_image_url(artist_name: str) -> tuple[None, None] | tuple[Any | None, Any, Any]:
+    if not artist_name:
+        return None, None
+
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyClientCredentials(
+            client_id=st.secrets["SPOTIPY_CLIENT_ID"],
+            client_secret=st.secrets["SPOTIPY_CLIENT_SECRET"],
+        )
+    )
+
+    result = sp.search(q=artist_name, type="artist", limit=1)
+    artists = result.get("artists", {}).get("items", [])
+
+    if not artists:
+        return None, None
+
+    artist = artists[0]
+    images = artist.get("images", [])
+    image_url = images[0]["url"] if images else None
+    spotify_url = artist["external_urls"]["spotify"]
+    return image_url, spotify_url
+
+def get_the_genre(track_url):
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyClientCredentials(
+            client_id=st.secrets["SPOTIPY_CLIENT_ID"],
+            client_secret=st.secrets["SPOTIPY_CLIENT_SECRET"],
+        )
+    )
+    track_info = sp.track(track_url)
+
+def get_total_days(data, year):
+    if 'mins' not in data.columns:
+        data['mins'] = (data['ms_played'] / 60000)
+    df_curr = data[data['ts_date'].dt.year == year]
+    total_mins = df_curr['mins'].sum()
+    total_days = total_mins / (24 * 60)
+
+    return total_days
+
+def group_by_month(data, sel_year):
     full_rng = pd.date_range(f'{sel_year}-01-01', f'{sel_year}-12-31')
-    df_plot = data[data['ts_date'].dt.year == sel_year].set_index('ts_date').reindex(full_rng, fill_value=0).reset_index(
-        names='date')
-
+    df_year = data[data['ts_date'].dt.year == sel_year].copy()
+    df_year['mins'] = (df_year['ms_played'] / 60000).round(0)
+    df_grouped = pd.DataFrame(df_year.groupby(df_year['ts_date'].dt.normalize())['mins'].sum())
+    df_plot = df_grouped.reindex(full_rng,fill_value=0).reset_index(names='date')
     df_plot['mo'] = df_plot['date'].dt.month
     df_plot['d'] = df_plot['date'].dt.dayofweek
     df_plot['w'] = df_plot['date'].dt.strftime('%W').astype(int)
     df_plot['w'] = df_plot['w'] - df_plot.groupby('mo')['w'].transform('min')
+    return df_plot
+
+def get_top_5_artists(data, sel_year, view_mode):
+    df = data[data['ts_date'].dt.year == sel_year].copy()
+    if view_mode in months:
+        month_num = months.index(view_mode) + 1
+        df = df[df['ts_date'].dt.month == month_num]
+        title = f"Top artists of {sel_year} ({view_mode})"
+    else:
+        title = f"Top artists of {sel_year}"
+    df = df.groupby(['master_metadata_album_artist_name']).size().reset_index(name='counts').sort_values(by='counts', ascending=False)
+    return df.head(6), title
+
+def get_top_5_songs(data, sel_year, view_mode, num = 5):
+    df = data[data['ts_date'].dt.year == sel_year].copy()
+    if view_mode in months:
+        month_num = months.index(view_mode) + 1
+        df = df[df['ts_date'].dt.month == month_num]
+        title = f"Top songs of {sel_year} ({view_mode})"
+    else:
+        title = f"Top songs of {sel_year}"
+    top = df.groupby(['master_metadata_track_name', 'master_metadata_album_artist_name']).size().reset_index(
+        name='counts').sort_values(by='counts', ascending=False)
+    return top.head(num), title
+
+def get_top_8_albums(data, sel_year, view_mode):
+    if view_mode in months:
+        month_num = months.index(view_mode) + 1
+        data = data[data['ts_date'].dt.month == month_num]
+        title = f"Top albums of {sel_year} ({view_mode})"
+    else:
+        title = f"Top albums of {sel_year}"
+
+    df = data[data['ts_date'].dt.year == sel_year].copy()
+    df = df.groupby(['master_metadata_album_album_name', 'master_metadata_album_artist_name']).size().reset_index(
+        name='counts').sort_values(by='counts', ascending=False)
+    return df.head(12), title
+
+def num_of_artists_listened(data, sel_year):
+    df = data[data['ts_date'].dt.year == sel_year].copy()
+    df = df.groupby(['master_metadata_album_artist_name']).size().reset_index()
+    return df.shape[0]
+
+def get_max_in_the_day(data, sel_year):
+    df = data[data['ts_date'].dt.year == sel_year].copy()
+
+    if df.shape[0] == 0:
+        return 0
+    daily_sum = df.groupby(df['ts_date'].dt.date)['ms_played'].sum()
+    daily_sum_df = daily_sum.reset_index(name='total_ms_played').sort_values(by=['total_ms_played'], ascending=False)
+    daily_sum_df['hours'] = daily_sum_df['total_ms_played'] / (1000 * 60 * 60)
+
+    return daily_sum_df.iloc[0]['hours']
+
+
+def chart_sum(data, sel_year, color):
+    df = group_by_month(data, sel_year)
+    sums = df.groupby('mo')['mins'].sum()
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=months,
+        y=sums,
+        mode='lines+markers',
+        line=dict(color=color, width=3),
+        marker=dict(size=8)
+    ))
+
+    fig.update_layout(
+        title=f"<b>Minutes Listened per Month ({sel_year})</b>",
+        title_font_color="white",
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        font=dict(color="#a0a0a0"),
+        legend=dict(
+            orientation="h",
+            yanchor="bottom", y=1.02,
+            xanchor="right", x=1
+        ),
+        margin=dict(l=20, r=20, t=50, b=20),
+        height=400
+    )
+
+    fig.update_yaxes(gridcolor='#333', zerolinecolor='#333', rangemode="tozero")
+    fig.update_xaxes(gridcolor='#333')
+
+    return fig
+
+
+
+def draw_chart(data, colors, sel_year, view_mode):
+    df_plot = group_by_month(data, sel_year)
 
     active_days = data[data['mins'] > 0]['mins']
     q20 = active_days.quantile(0.2)
